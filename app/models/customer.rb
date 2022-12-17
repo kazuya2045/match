8ab49dce8bf806336d646a8f1faa949db93bfbba
EC2 Default User <ec2-user@ip-172-31-13-36.ap-northeast-1.compute.internal>
2022-12-17 09:46:29 +0000

    実装3

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -15,6 +15,9 @@ class Customer < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  has_many :customer_rooms, dependent: :destroy
+  has_many :chats, dependent: :destroy
+
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
