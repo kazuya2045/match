34de4cc951fa6ccde97e8cdee14ab9f45d9a74ba
EC2 Default User <ec2-user@ip-172-31-13-36.ap-northeast-1.compute.internal>
2022-12-26 04:45:24 +0000

    実装11

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -27,7 +27,7 @@ class Admin::SessionsController < Devise::SessionsController
 
   protected
     def after_sign_in_path_for(resource)
-      admin_root_path
+      admin_customers_path
     end
 
     def after_sign_out_path_for(resource)
